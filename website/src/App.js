10a13d56e6ac362fd5a082a4e896ba27ad609626
Diogo Guimarães <diogoferreiraguima@hotmail.com>
2020-09-25 09:15:28 +0200

    Minor changes in the styles and initial mockups created

--- website/src/App.js
+++ website/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
+import profileImagePath from './images/profile.png'
+import linkedinImagePath from './images/icons/linkedin.png'
+import emailImagePath from './images/icons/email.png'
+import gitHubImagePath from './images/icons/github.png'
 import './App.css';
 
 function App() {
@@ -18,40 +21,35 @@ function App() {
               <a href='#'>Bons conteudos</a>
             </li>
           </ul>
-          <ul className='no-style-horizontal'>
+
+        </div>
+      </header>
+      <div className="body">
+        <div className='presentation'>
+          <h2>Diogo</h2>
+          <p>
+            Desenvolvedor full stack por profissão e desenvolvedor de jogos como hobby.
+        </p>
+          <ul className='no-style-horizontal social-media'>
             <li>
               <a href='mailto:diogoferreiraguima@hotmail.com'>
-                <img className='icon' src='./images/icons/email.png' />
+                <img className='icon' src={emailImagePath} />
               </a>
             </li>
             <li>
               <a href='https://www.linkedin.com/in/diogoafg/' target='new'>
-                <img className='icon' src='./images/icons/linkedin.png' />
+                <img className='icon' src={linkedinImagePath} />
               </a>
             </li>
             <li>
               <a href='https://github.com/dioguima' target='new'>
-                <img className='icon' src='./images/icons/github.png' />
+                <img className='icon' src={gitHubImagePath} />
               </a>
             </li>
           </ul>
         </div>
-        <ul className='no-style-horizontal'>
-          <li>
-            {/* <img className='profile' src='./profile.png' /> */}
-          </li>
-          <li>
-            <p className='title'>
-              Olá, <br />
-              eu sou o <span className='highlight'>Diogo</span>
-            </p>
-          </li>
-        </ul>
-      </header>
+      </div>
 
-      {/* <li> */}
-
-      {/* </li> */}
     </div>
   );
 }
