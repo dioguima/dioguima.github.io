406d1faf49ee5cd42e4d618d0447fb69a5e9f694
Diogo Guimarães <diogoferreiraguima@hotmail.com>
2020-10-25 20:38:33 +0100

    Updates

---
+++ precache-manifest.8fa9f9e31bae9649fb5e2c56a710506f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8357f664712c60ea11c751160278d413",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80ab354259d798950592",
+    "url": "/static/css/main.631c71e9.chunk.css"
+  },
+  {
+    "revision": "f1f75a6ae2597a54da03",
+    "url": "/static/js/2.08bf8ac1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.08bf8ac1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80ab354259d798950592",
+    "url": "/static/js/main.81d59823.chunk.js"
+  },
+  {
+    "revision": "ba126735810163618457",
+    "url": "/static/js/runtime-main.3374ac37.js"
+  },
+  {
+    "revision": "ae03b7677b16a79a495d905147c92c61",
+    "url": "/static/media/profile.ae03b767.png"
+  }
+]);
